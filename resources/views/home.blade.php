87f2381e65606dd6b05c20f2f3e111e03fddf83f
Madondo07 <dumisanemadondo926@gmail.com>
2026-05-20 17:28:15 +0200

    Fork testing

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,8 +1,5 @@
 @extends('layouts.app')
-<<<<<<< HEAD
-=======
-{{-- PRT362S — Eben Supply | Group KN3 --}}
->>>>>>> 09ba8fc83fc249adb6b17df0c29b3ac84fd4f4c5
+{{-- Dumisane Madondo 230949703 | Group KN3 --}}
 @section('title', 'Home')
 
 @section('content')
